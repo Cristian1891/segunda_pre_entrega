46de9089706ff8a5bbdbd1f7e9b751c36cdb05a4
Cristian1891 <cristianovejero1891@gmail.com>
2024-11-19 17:03:01 -0300

    preparado

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "https://cristian1891.github.io/segunda_pre_entrega"
+  plugins: [react()]
 })
